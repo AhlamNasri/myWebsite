13908effa236816ca7cf509612c8c087a7438457
AhlamNasri <ahlam2004nasri@gmail.com>
2025-09-10 16:28:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -264,10 +264,13 @@ app.use((req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    console.log('Upload directories created:', publicDirs.join(', '));
-    if (authRoutes) {
-        console.log('Auth routes loaded');
-    }
-});
+//app.listen(PORT, () => {
+    //console.log(`Server running on http://localhost:${PORT}`);
+   // console.log('Upload directories created:', publicDirs.join(', '));
+  //  if (authRoutes) {
+     //   console.log('Auth routes loaded');
+    //}
+
+//});
+
+module.exports = app;
